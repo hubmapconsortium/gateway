4cb652a552add620cf7b17ad43cfe966fb6a76f3
yuanzhou <yuanzhou19@gmail.com>
2019-10-31 10:40:53 -0400

    Bash script tweaks

--- hubmap-docker.sh
+++ hubmap-docker.sh
@@ -1,35 +1,41 @@
 #!/bin/bash
 
-# First create the shared docker network
-docker network create gateway_hubmap
+if [[ "$1" != "dev" && "$1" != "prod" ]]; then
+	echo "Unknown build environment '$1', specify either 'dev' or 'prod'"
+else
+	# First create the shared docker network
+	docker network create gateway_hubmap
 
-# Build images for each project
-cd ..
+	# Back to parent directory
+	cd ..
 
-cd uuid-api/docker
-./docker-setup.sh
-docker-compose -f docker-compose.yml -f docker-compose.$1.yml build
-docker-compose -p uuid-api_and_mysql -f docker-compose.yml -f docker-compose.$1.yml up -d
+	# Build images and spin up the containers for each project
+	cd uuid-api/docker
+	./docker-setup.sh
+	docker-compose -f docker-compose.yml -f docker-compose.$1.yml build
+	docker-compose -p uuid-api_and_mysql -f docker-compose.yml -f docker-compose.$1.yml up -d
 
-cd ../../
+	cd ../../
 
-cd entity-api/docker
-./docker-setup.sh
-docker-compose -f docker-compose.yml -f docker-compose.$1.yml build
-docker-compose -p entity-api_and_neo4j -f docker-compose.yml -f docker-compose.$1.yml up -d
+	cd entity-api/docker
+	./docker-setup.sh
+	docker-compose -f docker-compose.yml -f docker-compose.$1.yml build
+	docker-compose -p entity-api_and_neo4j -f docker-compose.yml -f docker-compose.$1.yml up -d
 
-cd ../../
+	cd ../../
 
-cd ingest-api/docker
-./docker-setup.sh
-docker-compose -f docker-compose.yml -f docker-compose.$1.yml build
-docker-compose -p ingest-api -f docker-compose.yml -f docker-compose.$1.yml up -d
+	cd ingest-ui/docker
+	./docker-setup.sh
+	docker-compose -f docker-compose.yml -f docker-compose.$1.yml build
+	docker-compose -p ingest-api -f docker-compose.yml -f docker-compose.$1.yml up -d
 
-cd ../../
+	cd ../../
 
-# The last one is gateway
-cd gateway
-docker-compose -f docker-compose.yml -f docker-compose.$1.yml build
-docker-compose -p gateway -f docker-compose.yml -f docker-compose.$1.yml up -d
+	# The last one is gateway since nginx conf files require entity-api, uuid-api, and ingest-api running
+	# before starting the gateway service
+	cd gateway
+	docker-compose -f docker-compose.yml -f docker-compose.$1.yml build
+	docker-compose -p gateway -f docker-compose.yml -f docker-compose.$1.yml up -d
 
+fi
 
