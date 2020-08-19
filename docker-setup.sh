6d6af678b24c1067bf1548acbea2d5831c6f1b6c
yuanzhou <yuanzhou19@gmail.com>
2020-08-18 23:14:31 -0400

    Added localhost individual build

--- docker-setup.sh
+++ docker-setup.sh
@@ -4,6 +4,5 @@
 # Version number is from the VERSION file
 # Also remove newlines and leading/trailing slashes if present in that VERSION file
 export HUBMAP_AUTH_VERSION=$(tr -d "\n\r" < VERSION | xargs)
-
 echo "HUBMAP_AUTH_VERSION: $HUBMAP_AUTH_VERSION"
 
