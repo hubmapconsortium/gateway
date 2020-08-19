25762d4963477efa8f68ec18ab06f8c853b4a193
yuanzhou <yuanzhou19@gmail.com>
2020-08-19 11:02:43 -0400

    Added down option

--- hubmap-auth-docker.sh
+++ hubmap-auth-docker.sh
@@ -3,8 +3,8 @@
 if [[ "$1" != "localhost" && "$1" != "dev" && "$1" != "test" && "$1" != "stage" && "$1" != "prod" ]]; then
     echo "Unknown build environment '$1', specify one of the following: 'localhost', 'dev', 'test', 'stage', or 'prod'"
 else
-    if [[ "$2" != "build" && "$2" != "start" && "$2" != "stop" && "$2" != "check" && "$2" != "config" ]]; then
-        echo "Unknown command '$2', specify 'build' or 'start' or 'stop' or 'check' or 'config' as the second argument"
+    if [[ "$2" != "build" && "$2" != "start" && "$2" != "stop" && "$2" != "down" && "$2" != "check" && "$2" != "config" ]]; then
+        echo "Unknown command '$2', specify 'build' or 'start' or 'stop' or 'down' or 'check' or 'config' as the second argument"
     else
         if [ "$2" = "build" ]; then
             # Use the `source` command to execute ./docker-setup.sh in the current process 
@@ -15,6 +15,8 @@ else
             docker-compose -p gateway -f docker-compose.yml -f docker-compose.$1.yml up -d
         elif [ "$2" = "stop" ]; then
             docker-compose -p gateway -f docker-compose.yml -f docker-compose.$1.yml stop
+        elif [ "$2" = "down" ]; then
+            docker-compose -p gateway -f docker-compose.yml -f docker-compose.$1.yml down
         elif [ "$2" = "check" ]; then
             # Bash array
             config_paths=(
