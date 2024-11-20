dcca16c6e4b169a920ae4569cfa750d0b4083e11
yuanzhou <zhy19@pitt.edu>
2024-11-19 19:49:11 -0500

    nginx tweaks

--- hubmap-auth/entrypoint.sh
+++ hubmap-auth/entrypoint.sh
@@ -16,25 +16,26 @@ echo "Starting hubmap-auth container with the same host user UID: $HOST_UID and 
 getent passwd $HOST_UID > /dev/null 2&>1
 # $? is a special variable that captures the exit status of last task
 if [ $? -ne 0 ]; then
-    groupadd -r -g $HOST_GID hubmap
-    useradd -r -u $HOST_UID -g $HOST_GID -m hubmap
+    groupadd -r -g $HOST_GID hive
+    useradd -r -u $HOST_UID -g $HOST_GID -m hive
 fi
 
 # When running Nginx as a non-root user, we need to create the pid file
-# and give read and write access to /var/run/nginx.pid, /var/cache/nginx, and /var/log/nginx
+# and give read and write access to a few directories
 # In individual nginx *.conf, also don't listen on ports 80 or 443 because 
 # only root processes can listen to ports below 1024
 touch /var/run/nginx.pid
-chown -R hubmap:hubmap /var/run/nginx.pid
-chown -R hubmap:hubmap /var/cache/nginx
-chown -R hubmap:hubmap /var/log/nginx
+chown -R hive:hive /var/run/nginx.pid
+chown -R hive:hive /var/cache/nginx
+chown -R hive:hive /var/log/nginx
+chown -R hive:hive /var/lib/nginx
 
 # No SSL in localhost mode
 if [ $DEPLOY_MODE != "localhost"  ]; then
-    chown -R hubmap:hubmap /etc/letsencrypt
+    chown -R hive:hive /etc/letsencrypt
 fi
 
 # Lastly we use su-exec to execute our process "$@" as that user
 # Remember CMD from a Dockerfile of child image gets passed to the entrypoint.sh as command line arguments
 # "$@" is a shell variable that means "all the arguments"
-exec /usr/local/bin/su-exec hubmap "$@"
+exec /usr/local/bin/su-exec hive "$@"
